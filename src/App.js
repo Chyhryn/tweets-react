893c6be589eb2e68778b2fa01a02161f3ce35ef6
Oleksandr Chyhryn <as.chigrin@gmail.com>
2023-04-21 15:56:30 +0300

    add / in path

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import { SharedLayout } from "./components/SharedLayout/SharedLayout";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { Tweets } from "./pages/Tweets/Tweets";
+import "./App.css";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="tweets" element={<Tweets />} />
+        <Route path="/tweets" element={<Tweets />} />
         <Route path="*" element={<HomePage />} />
       </Route>
     </Routes>
